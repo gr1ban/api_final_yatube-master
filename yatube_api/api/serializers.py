72c6143580994c82fabd8f2e1d6c38fe32fdf6a2
Sergey Griban <gr1ban.s@yandex.ru>
2024-05-27 17:24:56 +0300

    4

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Post, Group, Comment, Follow, User
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ['post']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(), fields=('following', 'user'),
+                message='Вы уже подписаны!')
+        ]
+
+    def validate_following(self, value):
+        if self.context['request'].user == value:
+            raise serializers.ValidationError('Невозможно оформить подписку'
+                                              'на самого себя.')
+        return value
